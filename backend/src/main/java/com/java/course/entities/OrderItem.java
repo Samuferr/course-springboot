11e9b3bffc52f8e18cac2836dc44f673c685d5c0
Samuferr <samuferr@gmail.com>
2021-05-30 17:21:41 -0300

    User insert

--- backend/src/main/java/com/java/course/entities/OrderItem.java
+++ backend/src/main/java/com/java/course/entities/OrderItem.java
@@ -20,6 +20,9 @@ public class OrderItem implements Serializable {
 	private Integer quantity;
 	private Double price;
 	
+	public OrderItem() {
+		
+	}
 	
 	public OrderItem(Order order, Product product, Integer quantity, Double price) {
 		super();
